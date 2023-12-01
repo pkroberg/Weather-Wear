538bbb7dd63b5fc7fcb9e0380dd74af67cfb2830
Parker <pkroberg@gmail.com>
2023-11-30 23:53:02 -0700

    Direct geocoding

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 //call current weather data
-let lat=37.7749; // example latitude
-let lon=-122.4194; // example longitude
+//api key e5bf64efcd979a72fe523a3c4c280da2
+let city="Fort Collins";
+let state="Colorado";
+let country="USA";
 
-let apiURL=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e5bf64efcd979a72fe523a3c4c280da2`;
+let apiURL=`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=5&appid=e5bf64efcd979a72fe523a3c4c280da2`;
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
+        let latitude=jsObject[0].lat;
+        let longitude=jsObject[0].lon;
+        console.log("Latitude:", latitude);
+        console.log("Longitude:", longitude);
     });
