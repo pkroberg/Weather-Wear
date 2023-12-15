9cf071f4336f217442b62cad4b30ba608c2066a2
Parker <pkroberg@gmail.com>
2023-12-14 23:45:57 -0700

    Add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports={
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'palette-green1': '#DAD7CD',
+        'palette-green2': '#A3B18A',
+        'palette-green3': '#588157',
+        'palette-green4': '#3A5A40',
+        'palette-green5': '#344E41',
+      }
+    },
   },
   plugins: [],
 }
